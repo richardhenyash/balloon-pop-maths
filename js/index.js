3184c14dcbb6670efd793855beb57756047b26af
richardhenyash <richardash1@gmail.com>
2021-02-24 09:09:25 +0000

    array shuffleArray function updated

--- js/index.js
+++ js/index.js
@@ -397,11 +397,10 @@ function returnTimesTableArray (tno) {
 /** Function to randomly shuffle an array*/
 /** https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array **/
 function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+    let j;
+    for (let i = array.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
     return(array);
 }
