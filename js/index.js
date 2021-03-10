e8dca93aa98c9e01b5530463296bc3f4f429bc8c
richardhenyash <richardash1@gmail.com>
2021-03-10 08:49:29 +0000

    Contact form connected to EmailJS service

--- js/index.js
+++ js/index.js
@@ -944,3 +944,37 @@ function initialiseBalloons() {
     drawBalloonImage("canvas-balloon-right-2", imgBalloonGreen, 1);
     drawBalloonImage("canvas-balloon-right-3", imgBalloonYellow, 1);
 }
+
+/** function sendMail(contactForm) {
+    console.log(contactForm.id);
+    console.log(contactForm.name.value);
+    console.log(contactForm.email.value);
+    console.log(contactForm.message.value);
+    return false;
+}
+
+/** Function to send email from modal contact form **/
+function sendMail(contactForm) {
+    emailjs.send("PersonalGmail","balloon-pop-maths",{
+        from_name: contactForm.name.value,
+        from_email: contactForm.email.value,
+        message: contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+            $("#modal-contact").modal('hide');
+            $("#modal-feedback-heading-text").text("Success!");
+            $("#modal-feedback-body-text").text("Your contact form was submitted succesfully.");
+            $('#modal-feedback').modal();
+        },
+        function(error) {
+            console.log("FAILED", error);
+            $("#modal-contact").modal('hide');
+            $("#modal-feedback-heading-text").text("Oops!");
+            $("#modal-feedback-body-text").text("Your contact form was not submitted.");
+            $('#modal-feedback').modal();
+        }
+    );
+    return false;  // To block from loading a new page
+}
