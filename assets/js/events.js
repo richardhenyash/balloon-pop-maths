b323df288264878794a50dbed3ca36196ea0be67
richardhenyash <richardash1@gmail.com>
2021-03-19 10:07:49 +0000

    In game options button renamed to Home, aria-label attributes added to buttons and button groups

--- assets/js/events.js
+++ assets/js/events.js
@@ -102,8 +102,8 @@ $("#balloon-answer-text-right-1").on("click", checkSelectedAnswer);
 $("#balloon-answer-text-right-2").on("click", checkSelectedAnswer);
 $("#balloon-answer-text-right-3").on("click", checkSelectedAnswer);
 
-// On click event added to game mode options button to hide game section and show heading and options sections //
-$("#btn-game-section-options").on("click", returnToMenu);
+// On click event added to game mode home button to hide game section and show heading and options sections //
+$("#btn-game-section-home").on("click", returnToMenu);
 
 // On click event added to options audio off button to mute audio //
 $("#audio-off").on("click", muteAudio);
